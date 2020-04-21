18f48ccd5b4bebbea64e1ad1c67d44d889aba3ec
praveenadhamodaran <41994920+praveenadhamodaran@users.noreply.github.com>
2020-04-21 18:08:25 +0530

    Add files via upload

---
+++ src/main/java/com/kgisl/springboottask/projectcrud/entity/User.java
@@ -0,0 +1,60 @@
+package com.kgisl.springboottask.projectcrud.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Team
+ */
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User() {
+
+    }
+  
+
+    
+    public Long getid() {
+        return id;
+    }
+    public void setid(Long id) {
+        this.id = id;
+    }
+
+    
+    public String getname() {
+        return name;
+    }
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getemail() {
+        return email;
+    }
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+
+public String getpassword() {
+        return password;
+    }
+    public void setpassword(String password) {
+        this.password = password;
+    }
+
+
+}
