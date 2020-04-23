7586bf598729ddc05f32f1731f9398fe7bd87df7
praveenadhamodaran <41994920+praveenadhamodaran@users.noreply.github.com>
2020-04-23 11:32:18 +0530

    Add files via upload

---
+++ src/main/java/com/kgisl/springboottask/projectcrud/service/UserService.java
@@ -0,0 +1,20 @@
+package com.kgisl.springboottask.projectcrud.service;
+
+
+import java.util.List;
+
+import com.kgisl.springboottask.projectcrud.entity.User;
+
+
+public interface UserService {
+
+    public User createUser(User user);
+    public List<User> getUsers();
+    public User findByUserId(Long id);
+    public User updateUser(Long id,User user);
+    public void deleteUserById(Long id); 
+    
+    public User Userlogin(User user);
+    // public void loginValidation(String name,String password);
+
+}
