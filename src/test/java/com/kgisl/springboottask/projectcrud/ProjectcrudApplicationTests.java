6bfee9295c418834e027d5bce514354b098b6ce6
praveenadhamodaran <41994920+praveenadhamodaran@users.noreply.github.com>
2020-04-28 23:21:11 +0530

    Add files via upload

---
+++ src/test/java/com/kgisl/springboottask/projectcrud/ProjectcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgisl.springboottask.projectcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
