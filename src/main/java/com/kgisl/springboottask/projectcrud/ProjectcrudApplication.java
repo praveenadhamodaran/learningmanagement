32945d40e87fc0ef2c1ff2e80cb5b2e2581ba9cd
praveenadhamodaran <41994920+praveenadhamodaran@users.noreply.github.com>
2020-04-28 23:18:30 +0530

    Add files via upload

---
+++ src/main/java/com/kgisl/springboottask/projectcrud/ProjectcrudApplication.java
@@ -0,0 +1,14 @@
+package com.kgisl.springboottask.projectcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @ComponentScan(“com.kgisl.springboottask.projectcrud”)
+@SpringBootApplication
+public class ProjectcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcrudApplication.class, args);
+	}
+
+}
