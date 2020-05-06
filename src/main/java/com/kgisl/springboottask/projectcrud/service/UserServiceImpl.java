32f347b2bc47bb944a9f6a98923e33bc1bdcb5e2
praveenadhamodaran <41994920+praveenadhamodaran@users.noreply.github.com>
2020-05-06 14:35:30 +0530

    Add files via upload

---
+++ src/main/java/com/kgisl/springboottask/projectcrud/service/UserServiceImpl.java
@@ -0,0 +1,83 @@
+package com.kgisl.springboottask.projectcrud.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.kgisl.springboottask.projectcrud.entity.User;
+import com.kgisl.springboottask.projectcrud.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    User username = new User();
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User username) {
+        String use=username.getUserEmail();
+    boolean isExists = userRepository.existsByuserEmail(use);
+
+    if (!isExists) {
+        username.setUserName(username.getUserName());
+        username.setUserEmail(username.getUserEmail());
+        username.setUserPassword(username.getUserPassword());
+                return userRepository.save(username);
+        }
+    return null;
+    }
+
+    @Override
+    public List<User> getUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findByUserId(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Not found"));
+    }
+
+    @Override
+    public User updateUser(Long id, User username) {
+        User t = userRepository.getOne(id);
+        t.setUserName(username.getUserName());
+t.setUserEmail(username.getUserEmail());
+        return userRepository.save(t);
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+
+    }
+
+    @Override
+    public User Userlogin(User username) {
+        int log;
+        // User username= new User();
+        String name=username.getUserName();
+        String password=username.getUserPassword();
+        boolean isExists = userRepository.existsByuserNameAndUserPassword(name,password);
+        if (!isExists) {
+            username=null;
+            }
+            else{
+                
+                username.setUserName(name);
+                username.setUserPassword(password);
+                // username.setConfirmPassword(username.getConfirmPassword);
+                // username.setUserEmail(username.getUserEmail);
+                // username.setUserId(username.getUserId);
+            }
+            return username;
+       
+       
+    }
+
+    
+}
